7aa32df3c3d67fe846f941ad95939e88522c2f9b
Giggy <71663261+GiggyMantis@users.noreply.github.com>
2024-03-03 11:06:19 -0500

    Text on svg generation

--- src/dendrogram.rs
+++ src/dendrogram.rs
@@ -1,7 +1,10 @@
 use std::cell::RefCell;
+use std::cmp;
 use std::rc::Rc;
 use svg::Document;
-use svg::node::element::{Circle, Path, path::Data};
+use svg::node::element::{Circle, Path, path::Data, Style};
+use svg::node::{Comment, element};
+use svg::node::element::tag::Style;
 use crate::model::age_of_common_ancestor;
 use crate::{model, TreeNode};
 
@@ -9,14 +12,20 @@ type DendrogramDocument = Document;
 pub type ConnectionTuple = (usize, usize, i32); // First Index (in stack), Second Index (in stack), Year they should be connected at
 
 pub fn generate(nodes: Vec<(String, i32)>, nodes_in_order: Vec<(String, i32)>, connections: &mut Vec<(usize, usize, i32)>) -> DendrogramDocument {
-    let mut ret: DendrogramDocument = Document::new().set("viewBox", (0, 0, (nodes.len()+5) * 10, model::RATE_OF_LANGUAGE_CHANGE / 3.5));
+    let mut ret: DendrogramDocument = Document::new().set("viewBox", (0, 0, 250 + (nodes.len()+5) * 10, model::RATE_OF_LANGUAGE_CHANGE / 3.5)).add(Style::new(
+        ".small {
+        font: bold 2.0px sans-serif;
+        }"
+    ));
     let mut data = Data::new();
     let mut stack: Vec<(f64, f64)> = Vec::new();
 
     for (_, &ref node) in nodes.iter().enumerate() {
         let i = nodes_in_order.iter().position(|r| r == node).unwrap();
-        let cord = ((5 + (i * 10)) as f64, get_y_from_year(nodes_in_order[i].1));
-        ret = ret.add(Circle::new().set("cx", cord.0).set("cy", cord.1).set("r", 1.2));
+        let cord = ((100 + (i * 10)) as f64, get_y_from_year(nodes_in_order[i].1));
+        let s = &nodes_in_order[i].0;
+        let length = f64::min(1.0 * (s.len() as f64), 13.0);
+        ret = ret.add(Comment::new(s)).add(Circle::new().set("cx", cord.0).set("cy", cord.1).set("r", 1.2)).add(element::Text::new(s).set("x", cord.0 - (length/2.0)).set("y", cord.1 - 2.0).set("class", "small").set("textLength", length));
         data = data.move_to((i, node.1));
         stack.push(cord);
     }
@@ -45,7 +54,7 @@ pub fn generate(nodes: Vec<(String, i32)>, nodes_in_order: Vec<(String, i32)>, c
         let p = Path::new()
             .set("fill", "none")
             .set("stroke", "black")
-            .set("stroke-width", 0.3)
+            .set("stroke-width", 0.5)
             .set("d", d);
 
         ret = ret.add(p);
